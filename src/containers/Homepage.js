a34b74341745d1ef0d38d2cdfb61b5a94cd60b27
Peyton Doyle <p6doyle@gmail.com>
2019-11-01 11:09:24 -0400

    working on home page styling

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,9 +1,66 @@
-import React from "react";
+// import React from "react";
+//
+// class Homepage extends React.Component {
+//   render() {
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">;
+//     return <h1>Hello, Peyton</h1>
+//   }
+// }
+//
+// export default Homepage;
 
-class Homepage extends React.Component {
-  render() {
-    return <h1>Hello, Peyton</h1>;
-  }
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function FullWidthGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+          <div id="maintitle">
+            <h1 id="title">Real<br></br>Housewives</h1>
+          </div>
+          <hr></hr>
+          <div id="subtitle">
+            <p>Most everything you’d want to know about the Real Housewives of New York & all of the other American housewife franchises.</p>
+          </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
-export default Homepage;
