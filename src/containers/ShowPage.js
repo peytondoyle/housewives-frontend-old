74e77b4fb9cca12cb25848fe44be3d22383dc56b
Peyton Doyle <p6doyle@gmail.com>
2019-11-10 11:31:57 -0500

    last tweak for add/delete like button

--- src/containers/ShowPage.js
+++ src/containers/ShowPage.js
@@ -137,26 +137,6 @@ class ShowPage extends React.Component {
           this.setState({currentRatingId: rating.id})})
     }
 
-//
-//     export function deleteCourse(formData) {
-//
-//   const configObj = {
-//     method: "DELETE",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json"
-//     },
-//   }
-//
-//   return dispatch => {
-//     dispatch({ type: 'DELETING_COURSE' });
-//     fetch(`http://localhost:3001/courses/${formData.courseId}`, configObj)
-//     .then(r => r.json())
-//     .then(deletedCourse => dispatch({ type: 'COURSE_DELETED', payload: deletedCourse }))
-//     .catch(error => alert(`There was an error (${error.message})`));
-//   }
-// }
-
     deleteLike = () => {
      let currentHW = this.props.selectedHW.id
      this.setState({liked: false, heartImg: "https://i.imgur.com/j3BRC9r.png"})
